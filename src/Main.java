5d875effb2947488d7661fd832fe2b69bf1231a7
mervedmrc <samanyolu.700@hotmail.com>
2014-07-08 09:14:55 +0300

    Main java gonderildi

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+
+        Kisi kisi = new Kisi();
+
+        kisi.setAd("Merve");
+        kisi.setSoyad("Demirci");
+        kisi.setYas(20);
+
+        kisi.yazdir();
+
     }
 }
